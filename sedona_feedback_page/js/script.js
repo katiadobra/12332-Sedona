d52c1856ef29b453db13f0d69bf925c54ee3d259
katiadobra <broun.kat@gmail.com>
2015-03-26 10:14:08 +0200

    counter-input

--- sedona_feedback_page/js/script.js
+++ sedona_feedback_page/js/script.js
@@ -4,18 +4,11 @@
   var minusCounter = document.querySelector('.counter-action.plus');
   var elements = document.querySelector('.counter-input');
   var currentvalue;
-  currentvalue = parseInt(elements.value);
 
   plusCounter.addEventListener('click', function(event) {
     event.preventDefault();
 
     currentvalue = parseInt(elements.value) + 1;
-    if (currentvalue < 1) {
-      currentvalue = 1
-    }
-    if (currentvalue > 30) {
-      currentvalue = 30
-    }
     document.querySelector('.travel-time').value = currentvalue;
   });
 
