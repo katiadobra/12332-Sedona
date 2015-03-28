bfb666e9658ec98749629afbf19ff0aa9bfac153
katiadobra <broun.kat@gmail.com>
2015-03-28 11:57:28 +0200

    plus/minus btns onclick

--- sedona_feedback_page/js/script.js
+++ sedona_feedback_page/js/script.js
@@ -58,23 +58,4 @@
     input.value = value;
   }
 
-  // var plusCounter = document.querySelector('.counter-action.minus');
-  // var minusCounter = document.querySelector('.counter-action.plus');
-  // var elements = document.querySelector('.counter-input');
-  // var currentvalue;
-
-  // plusCounter.addEventListener('click', function(event) {
-  //   event.preventDefault();
-
-  //   currentvalue = parseInt(elements.value) + 1;
-  //   document.querySelector('.travel-time').value = currentvalue;
-  // });
-
-  // minusCounter.addEventListener('click', function(event) {
-  //   event.preventDefault();
-
-  //   currentvalue = parseInt(elements.value) - 1;
-  //   document.querySelector('.travel-time').value = currentvalue;
-  // });
-
 })();
